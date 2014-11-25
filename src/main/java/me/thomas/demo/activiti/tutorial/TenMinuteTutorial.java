d1ba6d481aac1932d681782c97c0449b00f2a075
thomas <zhao.xshg47@gmail.com>
2014-11-25 17:52:46 +0800

    半途更改task的assignee

--- src/main/java/me/thomas/demo/activiti/tutorial/TenMinuteTutorial.java
+++ src/main/java/me/thomas/demo/activiti/tutorial/TenMinuteTutorial.java
@@ -2,6 +2,7 @@ package me.thomas.demo.activiti.tutorial;
 
 import org.activiti.engine.*;
 import org.activiti.engine.runtime.Execution;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 
 import java.util.HashMap;
@@ -12,20 +13,40 @@ public class TenMinuteTutorial {
 
     public static void main(String[] args) {
 
+        String hr = "celine";
+
         // Create Activiti process engine
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 
         // Get Activiti services
         RepositoryService repositoryService = processEngine.getRepositoryService();
-        RuntimeService runtimeService = processEngine.getRuntimeService();
+        RuntimeService runtimeService =  processEngine.getRuntimeService();
         TaskService taskService = processEngine.getTaskService();
 
         // Deploy process
+        String processDefinitionId = null;
         if (repositoryService.createProcessDefinitionQuery().processDefinitionKey("askForLeave").count() == 0) {
-            repositoryService.createDeployment().addClasspathResource("askForLeave.bpmn").deploy().getId();
+            processDefinitionId = repositoryService.createDeployment().addClasspathResource("askForLeave.bpmn").deploy().getId();
+        } else {
+            processDefinitionId = repositoryService.createProcessDefinitionQuery().processDefinitionKey("askForLeave").singleResult().getId();
         }
 
-        String processInstanceId = runtimeService.startProcessInstanceByKey("askForLeave").getId();
+        // when start the workflow, the hr is set to "celine"
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("startUser", "thomas");
+        variables.put("hr", hr);
+        String processInstanceId = runtimeService.startProcessInstanceById(processDefinitionId, variables).getId();
+        System.out.println("New instance under <askForLeave> => " + processInstanceId);
+
+        // Find all instances which belong to the specific process definition key
+        List<ProcessInstance> instances = runtimeService.createProcessInstanceQuery().processDefinitionId(processDefinitionId).list();
+        for (ProcessInstance instance : instances) {
+            System.out.println("All instances under <askForLeave> => " + instance.getId());
+            // here change "john" to do the second hr task
+            hr = "john";
+            Execution execution = runtimeService.createExecutionQuery().executionId(instance.getId()).singleResult();
+            runtimeService.setVariable(execution.getId(), "hr", hr);
+        }
 
         // Do the first task
         List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("manager").active().list();
@@ -38,9 +59,9 @@ public class TenMinuteTutorial {
         tasks = taskService.createTaskQuery().taskAssignee("thomas").active().list();
         System.out.println("thomas' tasks count => " + tasks.size());
         for (Task task : tasks) {
-            System.out.println("task.getId() => " + task.getId());
+            System.out.println("first task.getId() => " + task.getId());
             // complete it
-            Map<String, Object> variables = new HashMap<String, Object>();
+            variables = new HashMap<String, Object>();
             variables.put("approved", true);
 
             taskService.complete(task.getId(), variables);
@@ -64,8 +85,10 @@ public class TenMinuteTutorial {
             taskService.claim(task.getId(), "celine");
         }
         // Verify that celine can retrieve the task
-        tasks = taskService.createTaskQuery().taskAssignee("celine").active().list();
+        tasks = taskService.createTaskQuery().taskAssignee(hr).active().list();
+        System.out.println(hr + "'s tasks count => " + tasks.size());
         for (Task task : tasks) {
+            System.out.println("second task.getId() => " + task.getId());
             // complete it
             taskService.complete(task.getId());
         }
